a18b6a5084d052fab94e3a666af8e3f87ce78180
Glen Shewchuck <glenshewchuck@gmail.com>
2014-06-29 18:57:24 -0700

    commit on t1

--- index.php
+++ index.php
@@ -9,31 +9,12 @@
 
 </head>
 
-<?php
-  $xmldata = '<EarlyIQ>
-  <Action>
-    <GetInvestorStatus>
-      <FirstName>David</FirstName>
-      <LastName>Trissel</LastName>
-      <InvestorID>dtrissel+cert1219@earlyiq.com</InvestorID>
-    </GetInvestorStatus>
-  </Action>
-</EarlyIQ>';
-    $xml = simplexml_load_string($xmldata);
-?>
 
 <body>
 <p>&nbsp;</p>
-<p><center><a 
-href="https://www.earlyiq.com/investor-cert/HOqtHoLPvGsGH2teQbh4nM6qsF7WJ45KAUl9DAfhFKk|3D" 
-target="_new"><img 
-src="https://www.earlyiq.com/sites/all/themes/earlyiq/images/eiq_sprite.png" 
-/><br><span style="font-size:x-small;">Certificate</span></a></center></p>
+<p><center>back to clean index file with jquery</center</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
-<div id="report-card-link-div" style="display:none">
-  <div id="selectlink">TEST</div>
-  <p>Copy the selected image to your clipboard and then paste to email</p>
 </div>
 </body>
 <html>
